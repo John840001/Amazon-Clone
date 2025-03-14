67e27e8b3a8ed697df1ce7d4f2a250c6ae6edb5e
Jonathan Duarte <jonathanduarte840@gmail.com>
2025-03-15 02:19:01 +0530

    Added Routing for Products Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Layout from "./components/Layout.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const Home: React.FC = () => {
-  return <div className="text-2xl">Home</div>;
+// Pages
+import Home from "./pages/Home.tsx";
+import Products from "./pages/Products.tsx";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
-export default Home;
+export default App;
---
+++ frontend/src/pages/Products.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products: React.FC = () => {
+  return <div className="text-2xl">Products</div>;
+};
+
+export default Products;
